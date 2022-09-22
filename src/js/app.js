86f2f6386d99005c65b34044bf5ca50ba4ec56f1
Asset Kadyrzhanov <asset_kad@mail.ru>
2022-09-22 07:27:51 +0500

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from './modules/functions.js'
+
+flsFunctions.isWebp()
+flsFunctions.windowPopup()
+
+
+import Swiper, { Navigation, Pagination } from 'swiper'
+
+const swiper = new Swiper()
